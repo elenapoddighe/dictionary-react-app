b7c7fc023a99d9e2690053e218aa8f7cf551a27a
elenapoddighe <podd.elena@gmail.com>
2024-01-13 19:06:23 +0100

    Added phonetic part

--- src/Results.js
+++ src/Results.js
@@ -7,6 +7,9 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+
+        <h4>{props.results.phonetic}</h4>
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
