1cdb427842f28cbdc438521b15b8b42c777b460f
elenapoddighe <podd.elena@gmail.com>
2024-01-13 18:42:48 +0100

    Created Example and Synonyms components

---
+++ src/Example.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Example(props) {
+  if (props.example) {
+    return (
+      <div className="Example">
+        <strong>Example:</strong> {props.example}
+      </div>
+    );
+  }
+}
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Synonyms from "./Synonyms";
+import Example from "./Example";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -6,10 +8,12 @@ export default function Meaning(props) {
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       <div>
-        <p>{props.meaning.definition}</p>
-        <em>
-          <p>{props.meaning.example}</p>
-        </em>
+        <p>
+          <strong>Definition:</strong> {props.meaning.definition}
+          <br />
+          <Example example={props.meaning.example} />
+        </p>
+        <Synonyms synonyms={props.meaning.synonyms} />
       </div>
     </div>
   );
