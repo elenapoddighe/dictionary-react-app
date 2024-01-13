cf95b9af7fb26ea51b7cfc2644a64c89716bb4dc
elenapoddighe <podd.elena@gmail.com>
2024-01-13 15:21:52 +0100

    Built search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header>
+          <h1
+            className="text-center
+          "
+          >
+            Welcome to Elena's dictionary app
+          </h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <small>
+            Coded by Elena Poddighe, open-source on GitHub and hosted on Netlify
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
